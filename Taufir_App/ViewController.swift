40eed8b351a98f43d1005cedd195afe73b64dd0e
Saugaat Allabadi <SaugaatAllabadi@saugaats-macbook-air.local>
2017-02-08 17:08:28 +0400

    Initial Commit

---
+++ Taufir_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taufir_App
+//
+//  Created by Saugaat Allabadi on 2/8/17.
+//  Copyright © 2017 Taufir Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
