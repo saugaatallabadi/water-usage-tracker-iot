1d5f61d64b9b5ec645d8e0106439ed4e1fb63b63
saugaatallabadi <saugaatallabadi@gmail.com>
2018-08-23 12:40:35 +0400

    Newer comment.

--- FBSDKLoginKit.framework/Headers/FBSDKLoginButton.h
+++ FBSDKLoginKit.framework/Headers/FBSDKLoginButton.h
@@ -126,7 +126,7 @@
 // - Parameter loginButton: the sender
 // - Returns: YES if the login should be allowed to proceed, NO otherwise
 // */
-//Adding random comment.
+//Adding a change to newbranch
 //- (BOOL) loginButtonWillLogin:(FBSDKLoginButton *)loginButton;
 //
 //@end
